b1ea7ca49c26c4e9bb888d4e2f114ef032c40de0
rahilthehill <69742190+rahilthehill@users.noreply.github.com>
2023-01-25 13:34:40 +0800

    react starter, from threejsjourney

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.querySelector('#root'))
+
+root.render(
+    <h1>Hello React</h1> 
+    // this is not HTML, his is called jsx, its a tag based language that is similar to html
+    // it has features, not indent meaning, the indent doesn't matter for the code, unlike some
+    //
+)
